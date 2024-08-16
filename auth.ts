2950b75c0e0d516e9d452d4c8cc2a1af64fd0973
iamiboy <capitalcrptoss@gmail.com>
2024-08-16 05:12:30 -0700

    first commit

---
+++ auth.ts
@@ -0,0 +1,9 @@
+import NextAuth from "next-auth";
+import github from "next-auth/providers/github";
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  theme:{
+    logo: '/assets/images/logo.svg'
+  },
+  providers: [github],
+});
---
+++ components/shared/Header.tsx
@@ -0,0 +1,43 @@
+import Link from "next/link";
+import Image from "next/image";
+import { NAV_LINKS } from "@/constants";
+import { Button } from "../ui/button";
+import { auth } from "@/auth";
+
+const Header = async() => {
+     const session = await auth()
+     const user = session?.user
+
+  return (
+    <header className="w-full border-b">
+      <div className="wrapper flex items-center justify-between">
+        <Link href="/" className="w-36">
+          <Image
+            src="/assets/images/logo.svg"
+            width={128}
+            height={38}
+            alt="Evently logo"
+          />
+        </Link>
+        <ul className="hidden h-full gap-12 lg:flex">
+          {NAV_LINKS.map((link) => (
+            <Link
+              href={link.href}
+              key={link.key}
+              className="regular-16 text-b flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold"
+            >
+              {link.label}
+            </Link>
+          ))}
+        </ul>
+        <div className="flex w-32 justify-end gap-3">
+          <Button className="rounded-full w-24">
+            <Link href="api/auth/signin">Login</Link>
+          </Button>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
